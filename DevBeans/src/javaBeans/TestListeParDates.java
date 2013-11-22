775bf7b114144601eee04c6128e402137f1d8506
BonySimon <bony.simon77@gmail.com>
2013-11-22 15:22:03 +0100

    Update TestListeParDates.java

--- DevBeans/src/javaBeans/TestListeParDates.java
+++ DevBeans/src/javaBeans/TestListeParDates.java
@@ -1,21 +1,36 @@
 package javaBeans;
 
+import gestionErreurs.MessagesDErreurs;
+import gestionErreurs.TraitementException;
+
+/**
+ * This class tests the dates search functionality of BOperations
+ * @author BONY Simon - FELTZ Ludovic
+ */
 public class TestListeParDates {
 	
+	/**
+	 * This function is called to test the dates search functionality of BOperations
+	 * @param args parameters of this application. No utility here.
+	 */
 	public static void main(String[] args) {
-		BOperations bo = new BOperations();
-		bo.ouvrirConnexion();
-		bo.setNoDeCompte("0002");
-		bo.setDateInf("2013-10-15");
-		bo.setDateSup("2013-10-17");
-		bo.listerParDates();
-		for(String[] elems: bo.getOperationsParDates()){
-			System.out.print("->   ");
-			for(String elem: elems){
-				System.out.print(elem + " ");
-			}
-			System.out.print("\n");
-	    }
-		bo.fermerConnexion();
+		try{
+			BOperations bo = new BOperations();
+			bo.ouvrirConnexion();
+			bo.setNoDeCompte("0002");
+			bo.setDateInf("2013-10-15");
+			bo.setDateSup("2013-10-17");
+			bo.listerParDates();
+			for(String[] elems: bo.getOperationsParDates()){
+				System.out.print("->   ");
+				for(String elem: elems){
+					System.out.print(elem + " ");
+				}
+				System.out.print("\n");
+		    }
+			bo.fermerConnexion();
+		} catch (TraitementException e) {
+			System.out.println(MessagesDErreurs.getMessageDerreur(e.getMessage()));
+		}
 	}
 }
