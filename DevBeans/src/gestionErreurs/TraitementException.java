506e37321ebcf1b6e3dabd6600a359372097e277
BonySimon <bony.simon77@gmail.com>
2013-11-22 15:16:25 +0100

    Update TraitementException.java

--- DevBeans/src/gestionErreurs/TraitementException.java
+++ DevBeans/src/gestionErreurs/TraitementException.java
@@ -1,5 +1,17 @@
 package gestionErreurs;
 
+/**
+ * This class is a exception used to signal an error to search in MessagesDErreurs
+ * @author BONY Simon - FELTZ Ludovic
+ */
 public class TraitementException extends Exception{
 	private static final long serialVersionUID = 1L;
+
+	/**
+	 * This constructor instantiates this Exception with a code corresponding to the message to give
+	 * @param messageCode the code of the message to give
+	 */
+	public TraitementException(String messageCode) {
+		super(messageCode);
+	}
 }
