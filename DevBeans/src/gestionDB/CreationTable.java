469e5a949a2794716933bb4905ad877a1b97ebb7
BonySimon <bony.simon77@gmail.com>
2013-11-22 14:41:10 +0100

    Modifications finales

--- DevBeans/src/gestionDB/CreationTable.java
+++ DevBeans/src/gestionDB/CreationTable.java
@@ -1,5 +1,6 @@
 package gestionDB;
-import java.sql.SQLException;
+import gestionErreurs.MessagesDErreurs;
+import gestionErreurs.TraitementException;
 
 /**
  * This class contains a main method to create MySQL tables.
@@ -16,10 +17,8 @@ public class CreationTable
 	/**
 	 * This main method creates two tables into a MySQL Database: COMPTE and OPERATION
 	 * @param args An array containing command-line arguments
-	 * @throws SQLException An error occurs with SQL statements during the creation
-	 * @throws ClassNotFoundException An error occurs during the JDBC class initialization
 	 */
-	public static void main(String args[]) throws SQLException, ClassNotFoundException
+	public static void main(String args[]) 
 	{
 		String createCOMPTE = 
 				"CREATE TABLE IF NOT EXISTS COMPTE (" +
@@ -39,9 +38,14 @@ public class CreationTable
 		
 		// A MyDB object is used to open a connection and to create the tables 
 		MyDB db = new MyDB();
-		db.connect();
-		db.executeUpdate(createCOMPTE);
-		db.executeUpdate(createOPERATION);		
-		db.close();
+		
+		try {
+			db.connect();
+			db.executeUpdate(createCOMPTE);
+			db.executeUpdate(createOPERATION);		
+			db.close();
+		} catch (TraitementException e) {
+			MessagesDErreurs.getMessageDerreur(e.getMessage());
+		}
 	}
 }
