8009955b54769a2177ce98632a5aec30aa37d59f
BonySimon <bony.simon77@gmail.com>
2013-11-22 14:07:46 +0100

    Avec commentaires

--- DevBeans/src/gestionDB/CreationTable.java
+++ DevBeans/src/gestionDB/CreationTable.java
@@ -1,13 +1,26 @@
 package gestionDB;
 import java.sql.SQLException;
 
+/**
+ * This class contains a main method to create MySQL tables.
+ * This class cannot be instantiate
+ * @author BONY Simon - FELTZ Ludovic
+ */
 public class CreationTable 
 {
+	/**
+	 * This statement prevents the class from being instantiated
+	 */
+	private CreationTable(){}
+	
+	/**
+	 * This main method creates two tables into a MySQL Database: COMPTE and OPERATION
+	 * @param args An array containing command-line arguments
+	 * @throws SQLException An error occurs with SQL statements during the creation
+	 * @throws ClassNotFoundException An error occurs during the JDBC class initialization
+	 */
 	public static void main(String args[]) throws SQLException, ClassNotFoundException
 	{
-		MyDB db = new MyDB();
-		db.connect();
-		
 		String createCOMPTE = 
 				"CREATE TABLE IF NOT EXISTS COMPTE (" +
 				"	NOCOMPTE CHAR(4) NOT NULL, " +
@@ -24,6 +37,9 @@ public class CreationTable
 				"	OP CHAR(1) NOT NULL, " +
 				"	VALEUR DECIMAL(10,2) NOT NULL);";
 		
+		// A MyDB object is used to open a connection and to create the tables 
+		MyDB db = new MyDB();
+		db.connect();
 		db.executeUpdate(createCOMPTE);
 		db.executeUpdate(createOPERATION);		
 		db.close();
